212aa11b09412060bdd5e9951ae25fa0edd7bb89
Farley Diaz <jeinerf91@gmail.com>
2020-05-23 18:27:16 -0500

    archivo de configuracion

---
+++ compartido/config.php
@@ -0,0 +1,9 @@
+<?php
+  return (object) array(
+    'root_folder' => '/MISCELANEA-DALIS',
+    'db_server' => 'localhost',
+    'bd_user' => 'root',
+    'db_password' => '',
+    'db_name' => 'PAPELERIA_DALIS'
+  );
+?>
