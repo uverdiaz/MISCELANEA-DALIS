8e7374ca75b519a5951564a1164203beddce5576
Farley Diaz <jeinerf91@gmail.com>
2020-05-23 18:27:40 -0500

    header y nav aparte

---
+++ compartido/head.php
@@ -0,0 +1,11 @@
+<?php $config = include('config.php');?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="img/uver.jpg" type="image/x-icon">
+<!-- jquery -->
+<script  src="<?php echo $config->root_folder ?>/lib/jquery/jquery-3.1.1.min.js"></script>
+<!-- bootstrap -->
+<link rel="stylesheet" href="<?php echo $config->root_folder ?>/lib/bootstrap/dist/css/bootstrap.min.css">
+<script  src="<?php echo $config->root_folder ?>/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="<?php echo $config->root_folder ?>/css/estilo.css">
---
+++ compartido/nav.php
@@ -0,0 +1,27 @@
+<?php $config = include('config.php');?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Variedades DALIS J.E</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $config->root_folder ?>/index.php">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $config->root_folder ?>/papeleria.php">Papeleria</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $config->root_folder ?>/copisteria.php">copisteria</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $config->root_folder ?>/contacto.php">contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo $config->root_folder ?>/pedido.php">pedido</a>
+      </li>
+    </ul>
+  </div>
+</nav>
