ae74418a9c6b933496b3ef219821a6ee2ddcf3b8
uverdiaz <uverdiaz04@gmail.com>
2020-05-23 16:19:15 -0500

    Cambio titulo del index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Papeleria</title>
+    <title>Papeleria DALIS J.E</title>
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="shortcut icon" href="img/uver.jpg" type="image/x-icon">
 </head>
