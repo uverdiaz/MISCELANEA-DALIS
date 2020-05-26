d54e4e583d47a4ab01a2b07cadb285fe516ea90a
uverdiaz <uverdiaz04@gmail.com>
2020-05-26 16:27:20 -0500

    cambio icono pagina

--- compartido/head.php
+++ compartido/head.php
@@ -1,7 +1,7 @@
 <?php $config = include('config.php');?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" href="img/uver.jpg" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo $config->root_folder ?>/img/logo.png" type="image/x-icon">
 <!-- jquery -->
 <script  src="<?php echo $config->root_folder ?>/lib/jquery/jquery-3.1.1.min.js"></script>
 <!-- bootstrap -->
