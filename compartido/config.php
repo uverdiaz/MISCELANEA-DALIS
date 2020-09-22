593a7cb72d344632c7d17646efc1541dceeef9d0
uverdiaz <uverdiaz04@gmail.com>
2020-09-22 14:38:29 -0500

    Remodelacion y eliminacion de archivos

--- compartido/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  return (object) array(
-    'root_folder' => '/MISCELANEA-DALIS',
-    'db_server' => 'localhost',
-    'bd_user' => 'root',
-    'db_password' => '',
-    'db_name' => 'PAPELERIA_DALIS'
-  );
-?>
