19f0ffe5c438b3a4f2a766d3f990ddf8bf3db5e7
uver <uverdiaz202@gmail.com>
2022-12-09 07:58:26 -0500

    d

--- vista/paginas/entrada.php
+++ vista/paginas/entrada.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MENU PRINCIPAL</title>
+    <title>MENU DE REPORTES</title>
     <?php include("diseño/head.php") ?>
 </head>
 
@@ -10,7 +10,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#"></a>
+    <a class="navbar-brand" href="#">PerCol-XYZ</a>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03" style="display: flex;">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0 " style="text-align: left;
